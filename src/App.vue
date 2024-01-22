c40bdd643b042abc436b08d47cfe118f3a02942d
Vinffs <vin.demiranda@gmail.com>
2024-01-22 09:58:03 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <HeaderComponent />
+</template>
+
+<script>
+import HeaderComponent from "./components/HeaderComponent.vue";
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent,
+  },
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@use "./assets/styles/partials/variables" as *;
+</style>
